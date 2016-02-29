df6d2fb0fc4e96679f13c3e9e48548f17cc5425f
Nathan Bland <nathan.bland@gmail.com>
2016-02-29 11:05:41 -0700

    adding basic build

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express')        // call express
+var app = express()                 // define our app using express
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+var port = process.env.PORT || 8080
+
+var router = express.Router()
+
+router.get('/', function(req, res, next) {
+  console.log('get request', req.query)
+  var data = req.query
+  return res.json({ message: 'hooray! welcome to our api!', requested: data })   
+})
+router.post('/', function(req, res, next) {
+  console.log('post request', req.body)
+  var data = req.body 
+  return res.json({ message: 'hooray! welcome to our api!', requested: data })   
+})
+
+app.use(router)
+
+app.listen(port)
+console.log('Magic happens on port ' + port)
